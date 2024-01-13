4a61a03d411c49fbb47350827dec8d72dd10f92e
Motyak <m0tyak_@live.fr>
2024-01-13 17:57:25 +0100

    started refactoring tests with montree

--- src/test/SquareBracketsGroup_Strictly.cpp
+++ src/test/SquareBracketsGroup_Strictly.cpp
@@ -2,223 +2,200 @@
 
 #include <SquareBracketsGroup.h>
 #include <ParenthesesGroup.h>
+#include <utils/str-utils.h>
 
 TEST_CASE("no SquareBracketsGroup expression found", "[test-1000][strictly]")
 {
-    std::istringstream iss(
-        "fds"
-    );
+    auto iss = std::istringstream("fds");
 
     auto ret = tryConsumeSquareBracketsGroup(iss);
 
-    REQUIRE (!ret);
+    REQUIRE (false == ret.has_value());
 }
 
 TEST_CASE("err-105 premature EOF after SquareBracketsGroup's initiator", "[test-1001][strictly]")
 {
-    std::istringstream iss(
-        "["
-    );
+    auto iss = std::istringstream("[");
 
     auto ret = tryConsumeSquareBracketsGroup(iss);
 
-    REQUIRE (ret);
-    REQUIRE (!*ret);
-    REQUIRE (ret->error().code == 105);
+    REQUIRE (true == ret.has_value());
+
+    auto expect = tommy_str(R"END(
+        -> SquareBracketsGroup: `[`
+          -> ERR-105
+    )END");
+    REQUIRE (expect == montree::astToString(ret));
 }
 
-TEST_CASE("err-114 invalid program word", "[test-1004][strictly]")
-{
-    std::istringstream iss(
-        "]"
-    );
+// TEST_CASE("err-114 invalid program word", "[test-1004][strictly]")
+// {
+//     std::istringstream iss(
+//         "]"
+//     );
 
-    auto ret = consumeProgramWord(iss);
+//     auto ret = consumeProgramWord(iss);
 
-    REQUIRE (!ret);
-    REQUIRE (ret.error().code == 114);
+//     REQUIRE (!ret);
+//     REQUIRE (ret.error().code == 114);
 
-    REQUIRE (ret.error().cause);
-    REQUIRE (ret.error().cause.value()->code == 118);
-}
+//     REQUIRE (ret.error().cause);
+//     REQUIRE (ret.error().cause.value()->code == 118);
+// }
 
 TEST_CASE("empty group", "[test-1002][strictly]")
 {
-    std::istringstream iss(
-        "[]"
-    );
+    auto iss = std::istringstream("[]");
 
     auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (*ret);
-    auto sbg = std::get<SquareBracketsGroup*>(**ret);
-    REQUIRE (sbg->terms.size() == 0);
+
+    REQUIRE (true == ret.has_value());
+
+    auto expect = tommy_str(R"END(
+        -> SquareBracketsGroup: `[]`
+    )END");
+    REQUIRE (expect == montree::astToString(ret));
 }
 
-TEST_CASE("err-109 was expecting SquareBracketsGroup's terminator", "[test-1005][strictly]")
-{
-    std::istringstream iss(
-        "[fds"
-    );
+// TEST_CASE("err-109 was expecting SquareBracketsGroup's terminator", "[test-1005][strictly]")
+// {
+//     std::istringstream iss(
+//         "[fds"
+//     );
     
-    auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (!*ret);
-    REQUIRE (ret->error().code == 109);
-}
+//     auto ret = tryConsumeSquareBracketsGroup(iss);
+//     REQUIRE (ret);
+//     REQUIRE (!*ret);
+//     REQUIRE (ret->error().code == 109);
+// }
 
-TEST_CASE("err-106 was expecting a valid Term in SquareBracketsGroup", "[test-1007][strictly]")
-{
-    std::istringstream iss(
-        "[fds)"
-    );
+// TEST_CASE("err-106 was expecting a valid Term in SquareBracketsGroup", "[test-1007][strictly]")
+// {
+//     std::istringstream iss(
+//         "[fds)"
+//     );
 
-    auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
+//     auto ret = tryConsumeSquareBracketsGroup(iss);
+//     REQUIRE (ret);
 
-    REQUIRE (!*ret);
-    REQUIRE (ret->error().code == 106);
+//     REQUIRE (!*ret);
+//     REQUIRE (ret->error().code == 106);
 
-    REQUIRE (ret->error().cause);
-    REQUIRE (ret->error().cause.value()->code == 116);
+//     REQUIRE (ret->error().cause);
+//     REQUIRE (ret->error().cause.value()->code == 116);
 
-    REQUIRE (ret->error().cause.value()->cause);
-    REQUIRE (ret->error().cause.value()->cause.value()->code == 101);
-}
+//     REQUIRE (ret->error().cause.value()->cause);
+//     REQUIRE (ret->error().cause.value()->cause.value()->code == 101);
+// }
 
-TEST_CASE("err-107 was expecting SquareBracketsGroup's continuator", "[test-1003][strictly]")
-{
-    std::istringstream iss(
-        "[fds,"
-    );
+// TEST_CASE("err-107 was expecting SquareBracketsGroup's continuator", "[test-1003][strictly]")
+// {
+//     std::istringstream iss(
+//         "[fds,"
+//     );
 
-    auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
+//     auto ret = tryConsumeSquareBracketsGroup(iss);
+//     REQUIRE (ret);
 
-    REQUIRE (!*ret);
-    REQUIRE (ret->error().code == 107);
+//     REQUIRE (!*ret);
+//     REQUIRE (ret->error().code == 107);
 
-    REQUIRE (ret->error().cause);
-    REQUIRE (ret->error().cause.value()->code == 101);
-}
+//     REQUIRE (ret->error().cause);
+//     REQUIRE (ret->error().cause.value()->code == 101);
+// }
 
-TEST_CASE("err-108 was expecting another ProgramWord after SquareBracketsGroup's continuator", "[test-1008][strictly]")
-{
-    std::istringstream iss(
-        "[fds, ]"
-    );
+// TEST_CASE("err-108 was expecting another ProgramWord after SquareBracketsGroup's continuator", "[test-1008][strictly]")
+// {
+//     std::istringstream iss(
+//         "[fds, ]"
+//     );
 
-    auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (!*ret);
-    REQUIRE (ret->error().code == 108);
-}
+//     auto ret = tryConsumeSquareBracketsGroup(iss);
+//     REQUIRE (ret);
+//     REQUIRE (!*ret);
+//     REQUIRE (ret->error().code == 108);
+// }
 
-TEST_CASE("single atom", "[test-1009][strictly]")
-{
-    std::istringstream iss(
-        "[fds]"
-    );
+// TEST_CASE("single atom", "[test-1009][strictly]")
+// {
+//     std::istringstream iss(
+//         "[fds]"
+//     );
 
-    auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (*ret);
+//     auto ret = tryConsumeSquareBracketsGroup(iss);
+//     REQUIRE (ret);
+//     REQUIRE (*ret);
 
-    auto sbg = std::get<SquareBracketsGroup*>(**ret);
-    REQUIRE (sbg->terms.size() == 1);
+//     auto sbg = std::get<SquareBracketsGroup*>(**ret);
+//     REQUIRE (sbg->terms.size() == 1);
 
-    auto term1 = sbg->terms[0];
-    REQUIRE (term1.words.size() == 1);
+//     auto term1 = sbg->terms[0];
+//     REQUIRE (term1.words.size() == 1);
 
-    auto term1_word1 = std::get<Atom>(term1.words[0]);
-    REQUIRE (term1_word1.value == "fds");
-}
+//     auto term1_word1 = std::get<Atom>(term1.words[0]);
+//     REQUIRE (term1_word1.value == "fds");
+// }
 
 TEST_CASE("multiple terms, multiple atoms", "[test-1010][strictly]")
 {
-    std::istringstream iss(
-        "[3, 1 + 2]"
-    );
-    
-    auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (*ret);
-
-    auto sbg = std::get<SquareBracketsGroup*>(**ret);
-    REQUIRE (sbg->terms.size() == 2);
-
-    auto term1 = sbg->terms[0];
-    REQUIRE (term1.words.size() == 1);
-    auto term2 = sbg->terms[1];
-    REQUIRE (term2.words.size() == 3);
-
-    auto term1_word1 = std::get<Atom>(term1.words[0]);
-    REQUIRE (term1_word1.value == "3");
-    auto term2_word1 = std::get<Atom>(term2.words[0]);
-    REQUIRE (term2_word1.value == "1");
-    auto term2_word2 = std::get<Atom>(term2.words[1]);
-    REQUIRE (term2_word2.value == "+");
-    auto term2_word3 = std::get<Atom>(term2.words[2]);
-    REQUIRE (term2_word3.value == "2");
-}
-
-TEST_CASE("nested square brackets group", "[test-1011][strictly]")
-{
-    std::istringstream iss(
-        "[[fds]]"
-    );
+    auto iss = std::istringstream("[3, 1 + 2]");
 
     auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (*ret);
 
-    auto sbg = std::get<SquareBracketsGroup*>(**ret);
-    REQUIRE (sbg->terms.size() == 1);
+    REQUIRE (true == ret.has_value());
 
-    auto term1 = sbg->terms[0];
-    REQUIRE (term1.words.size() == 1);
-
-    auto term1_word1 = std::get<SquareBracketsGroup*>(term1.words[0]);
-    REQUIRE (term1_word1->terms.size() == 1);
-
-    auto term1_word1_term1 = term1_word1->terms[0];
-    REQUIRE (term1_word1_term1.words.size() == 1);
-
-    auto term1_word1_term1_word1 = std::get<Atom>(term1_word1_term1.words[0]);
-    REQUIRE (term1_word1_term1_word1.value == "fds");
-
+    auto expect = tommy_str(R"END(
+        -> SquareBracketsGroup: `[3, 1 + 2]`
+          -> term 1 -> one word: Atom: `3`
+          -> term 2
+            -> word 1: Atom: `1`
+            -> word 2: Atom: `+`
+            -> word 3: Atom: `2`
+    )END");
+    REQUIRE (expect == montree::astToString(ret));
 }
+
+// TEST_CASE("nested square brackets group", "[test-1011][strictly]")
+// {
+//     std::istringstream iss(
+//         "[[fds]]"
+//     );
+
+//     auto ret = tryConsumeSquareBracketsGroup(iss);
+//     REQUIRE (ret);
+//     REQUIRE (*ret);
+
+//     auto sbg = std::get<SquareBracketsGroup*>(**ret);
+//     REQUIRE (sbg->terms.size() == 1);
+
+//     auto term1 = sbg->terms[0];
+//     REQUIRE (term1.words.size() == 1);
+
+//     auto term1_word1 = std::get<SquareBracketsGroup*>(term1.words[0]);
+//     REQUIRE (term1_word1->terms.size() == 1);
+
+//     auto term1_word1_term1 = term1_word1->terms[0];
+//     REQUIRE (term1_word1_term1.words.size() == 1);
+
+//     auto term1_word1_term1_word1 = std::get<Atom>(term1_word1_term1.words[0]);
+//     REQUIRE (term1_word1_term1_word1.value == "fds");
+
+// }
 
 TEST_CASE("parentheses group", "[test-1012][strictly]")
 {
-    std::istringstream iss(
-        "[(a, b, c)]"
-    );
+    auto iss = std::istringstream("[(a, b, c)]");
 
     auto ret = tryConsumeSquareBracketsGroup(iss);
-    REQUIRE (ret);
-    REQUIRE (*ret);
 
-    auto sbg = std::get<SquareBracketsGroup*>(**ret);
-    REQUIRE (sbg->terms.size() == 1);
+    REQUIRE (true == ret.has_value());
 
-    auto term1 = sbg->terms[0];
-    REQUIRE (term1.words.size() == 1);
-
-    auto term1_word1 = std::get<ParenthesesGroup*>(term1.words[0]);
-    REQUIRE (term1_word1->terms.size() == 3);
-
-    auto term1_word1_term1 = term1_word1->terms[0];
-    REQUIRE (term1_word1_term1.words.size() == 1);
-    auto term1_word1_term2 = term1_word1->terms[1];
-    REQUIRE (term1_word1_term2.words.size() == 1);
-    auto term1_word1_term3 = term1_word1->terms[2];
-    REQUIRE (term1_word1_term3.words.size() == 1);
-
-    auto term1_word1_term1_word1 = std::get<Atom>(term1_word1_term1.words[0]);
-    REQUIRE (term1_word1_term1_word1.value == "a");
-    auto term1_word1_term2_word1 = std::get<Atom>(term1_word1_term2.words[0]);
-    REQUIRE (term1_word1_term2_word1.value == "b");
-    auto term1_word1_term3_word1 = std::get<Atom>(term1_word1_term3.words[0]);
-    REQUIRE (term1_word1_term3_word1.value == "c");
+    auto expect = tommy_str(R"END(
+        -> SquareBracketsGroup: `[(a, b, c)]`
+          -> one term -> one word: ParenthesesGroup: `(a, b, c)`
+            -> term 1 -> one word: Atom: `a`
+            -> term 2 -> one word: Atom: `b`
+            -> term 3 -> one word: Atom: `c`
+    )END");
+    REQUIRE (expect == montree::astToString(ret));
 }
