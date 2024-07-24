02df691f72803ad70f03088dc58785a21374d1ab
Motyak <m0tyak_@live.fr>
2024-07-24 22:36:13 +0200

    added sbg tests

--- src/test/sbg.cpp
+++ src/test/sbg.cpp
@@ -22,7 +22,7 @@ TEST_CASE ("empty square brackets group", "[test-4311][sbg]") {
 
 ////////////////////////////////////////////////////////////////
 
-TEST_CASE ("single element square brackets group", "[test-4312][sbg]") {
+TEST_CASE ("one one-word term in square brackets group", "[test-4312][sbg]") {
     auto input = "[fds]";
     
     auto expect = tommy_str(R"EOF(
@@ -40,7 +40,7 @@ TEST_CASE ("single element square brackets group", "[test-4312][sbg]") {
 
 ////////////////////////////////////////////////////////////////
 
-TEST_CASE ("multi elements square brackets group", "[test-4313][sbg]") {
+TEST_CASE ("two one-word terms in square brackets group", "[test-4313][sbg]") {
     auto input = "[fds, sdf]";
     
     auto expect = tommy_str(R"EOF(
@@ -49,6 +49,47 @@ TEST_CASE ("multi elements square brackets group", "[test-4313][sbg]") {
        |    -> Word: Atom: `fds`
        |  -> Term #2
        |    -> Word: Atom: `sdf`
+    )EOF");
+
+    auto input_iss = std::istringstream(input);
+    auto output = consumeSquareBracketsGroup(input_iss);
+    auto output_word = mayfail_convert<Word>(output);
+    auto output_str = montree::astToString(output_word);
+    REQUIRE (output_str == expect);
+}
+
+////////////////////////////////////////////////////////////////
+
+TEST_CASE ("one two-words term in square brackets group", "[test-4313][sbg]") {
+    auto input = "[fds sdf]";
+    
+    auto expect = tommy_str(R"EOF(
+       |-> SquareBracketsGroup
+       |  -> Term
+       |    -> Word #1: Atom: `fds`
+       |    -> Word #2: Atom: `sdf`
+    )EOF");
+
+    auto input_iss = std::istringstream(input);
+    auto output = consumeSquareBracketsGroup(input_iss);
+    auto output_word = mayfail_convert<Word>(output);
+    auto output_str = montree::astToString(output_word);
+    REQUIRE (output_str == expect);
+}
+
+////////////////////////////////////////////////////////////////
+
+TEST_CASE ("two two-words terms in square brackets group", "[test-4313][sbg]") {
+    auto input = "[fds sdf, abc def]";
+    
+    auto expect = tommy_str(R"EOF(
+       |-> SquareBracketsGroup
+       |  -> Term #1
+       |    -> Word #1: Atom: `fds`
+       |    -> Word #2: Atom: `sdf`
+       |  -> Term #2
+       |    -> Word #1: Atom: `abc`
+       |    -> Word #2: Atom: `def`
     )EOF");
 
     auto input_iss = std::istringstream(input);
