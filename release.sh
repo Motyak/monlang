b03f73f42beaf27b8d0768b464f885298a91af0e
Motyak <m0tyak_@live.fr>
2024-10-20 10:29:11 +0200

    better release sh

--- release.sh
+++ release.sh
@@ -2,6 +2,10 @@
 shopt -s nullglob globstar
 set -o errexit -o pipefail
 
+## we expect at least one header file and one release obj file ##
+(shopt -s failglob; : include/monlang/**.h)
+(shopt -s failglob; : obj/release/**.o)
+
 exec 9> /tmp/monlang_trace.txt
 export BASH_XTRACEFD=9
 trap '{ echo RELEASE FAILED, trace was saved in /tmp/monlang_trace.txt; } 9>/dev/null' ERR
@@ -10,11 +14,11 @@ set -o xtrace
 rm -rf dist/monlang-parser; mkdir -p $_
 
 ## package release objects (in background job) ##
-ar rcs dist/monlang-parser.a obj/release/**/*.o & package_proc_id=$!
+ar rcs dist/monlang-parser.a obj/release/**.o & package_proc_id=$!
 
 ## copy public header files ##
 cp -r include/monlang/* -t dist/monlang-parser/
-for header in dist/monlang-parser/**/*.h; do
+for header in dist/monlang-parser/**.h; do
     perl -i -pe 's/<monlang\//<monlang-parser\//g' $header;
 done
 
