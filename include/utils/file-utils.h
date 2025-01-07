c05283e57b87cc511aa946ef7b4b952bbddb4954
Motyak <m0tyak_@live.fr>
2025-01-07 17:53:18 +0100

    fix str-utils, added fs-utils, etc..

--- include/utils/file-utils.h
+++ include/utils/file-utils.h
@@ -1,11 +1,28 @@
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
-inline std::string slurp_file(std::string filepath) {
-    std::ifstream ifs(filepath);
-    std::ostringstream oss;
-    oss << ifs.rdbuf();
-    return oss.str();
+#include <fstream>
+
+class CantOpenFileException : std::exception {
+  public:
+    std::string filename;
+    CantOpenFileException(const std::string& filename) : std::exception(), filename(filename){}
+    const char* what() const noexcept override {
+        return ("Failed to open file `" + filename + "`").c_str();
+    }
+};
+
+inline std::string slurp_file(std::string filename) {
+    auto file = std::ifstream(filename);
+
+    if (!file.is_open()) {
+        throw CantOpenFileException(filename);
+    }
+
+    return std::string(
+        std::istreambuf_iterator<char>(file),
+        std::istreambuf_iterator<char>()
+    );
 }
 
 #endif // FILE_UTILS_H
